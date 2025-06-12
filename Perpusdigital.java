39809a7d568482e2c0299ecaeeb06713b1b0b764
milaadeliaa16 <milaadeliads@gmail.com>
2025-06-12 12:48:48 +0700

    Add files via upload

---
+++ Perpusdigital.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package perpusdigital;
+
+/**
+ *
+ * @author Hilman
+ */
+public class Perpusdigital {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        screen screenFrame = new screen();
+        screenFrame.setVisible(true);
+        screenFrame.pack();
+        screenFrame.setLocationRelativetTo(null);
+    }
+    
+}
